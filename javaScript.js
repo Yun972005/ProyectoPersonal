d051cc33ba44760aa46b8682e48da9aec043c9be
pau <pau>
2025-11-28 19:49:33 +0100

    segundo commit

---
+++ javaScript.js
@@ -0,0 +1,11 @@
+function horaActual(){
+    let fecha= new date();
+    let hora = date.getHoras();
+    let minutos = date.getMinutos();
+    let segundos = date.getSegundos();
+
+
+    let tiempoEnPantalla = hora + ":" +minutos + ":" + "segundos";
+
+    //juntarlo ahora con el html
+}
